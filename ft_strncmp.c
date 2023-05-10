27309f2ccc1777138683ad69975e9b04d6f70c11
estebanarriaga <estebanarriaga200@gmail.com>
2023-05-10 18:31:23 +0200

    Unused var res

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -15,10 +15,8 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
-	int		res;
 
 	i = 0;
-	res = 0;
 	while (s1[i] != '\0' && i < n && s1[i] == s2[i])
 		i ++;
 	if (i == n)
